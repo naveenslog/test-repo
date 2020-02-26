7ab5be0e7ef2cf829c4f175dd74007ccfb5fcfa1
Naveen Singh <na.singhall@gmail.com>
2020-02-26 23:47:59 +0530

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -55,5 +55,8 @@ $app->configure('apidoc');
 | from the actual running of the application and sending responses.
 |
 */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 return $app;
