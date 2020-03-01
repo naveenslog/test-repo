25be1fb2cc53f46d5ff6951b0aa1f714e2088914
Naveen Singh <na.singhall@gmail.com>
2020-03-01 07:00:18 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -67,7 +67,7 @@ class UserController extends Controller
 	{
         $user = Auth::user();
         if($user["user_type"] == "school"){
-            $details = Auth::user()->join('schoolprofile', "user.id" "=" "schoolprofile.user_id")->select("user.*", "schoolprofile.id");
+            $details = Auth::user()->join('schoolprofile', 'user.id' '=' 'schoolprofile.user_id')->select('user.*', 'schoolprofile.id');
             return response()->json(['success' => $user], $this->successStatus);
         }
         if($user["user_type"] == "student"){
