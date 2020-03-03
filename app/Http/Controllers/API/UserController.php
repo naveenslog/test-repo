6772170a9e12091fb7d608ff49d53819475f9033
Naveen Singh <na.singhall@gmail.com>
2020-03-03 13:26:09 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -527,7 +527,7 @@ class UserController extends Controller
         
         
         $get_school = DB::table('users as u')              
-            ->select('u.id','u.name','u.email','u.user_type','sp.name as school_name','sp.about','sp.email as school_email','sp.phone','sp.add_line1','sp.add_line2','sp.area_code','u.is_block') 
+            ->select('u.id','u.name', 'u.is_valid' , 'u.is_paid', 'u.email','u.user_type','sp.name as school_name','sp.about','sp.email as school_email','sp.phone','sp.add_line1','sp.add_line2','sp.area_code','u.is_block') 
             ->leftJoin('schoolprofile as sp', 'sp.user_id', '=', 'u.id')     
             ->where('u.user_type','school')
             ->offset($offset)
@@ -544,7 +544,9 @@ class UserController extends Controller
                 $response[$key]['about'] = $value->about;
                 $response[$key]['school_email'] = $value->school_email;
                 $response[$key]['phone'] = $value->phone;
-$response[$key]['is_block'] = $value->is_block==0?"Un Block":"Block";
+                $response[$key]['is_block'] = $value->is_block==0?"Un Block":"Block";
+                $response[$key]['is_valid'] = $value->is_valid;
+                $response[$key]['is_paid'] = $value->is_paid;
                 //$response[$key]['add_line1'] = $value->add_line1;
                 //$response[$key]['add_line2'] = $value->add_line2;
                 //$response[$key]['area_code'] = $value->area_code;
