581fd881607e1e265f454051ac3fc60673336200
Akash Mishra <akashmishra0208@gmail.com>
2020-03-06 19:06:44 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -206,7 +206,7 @@ class UserController extends Controller
                         ->where('sp.id', $id)->first();
 		if (!empty($schoolprofile)) 
 		{
-			$school_images = DB::table('school_images')->where('id', $id)->get();
+			$school_images = DB::table('school_images')->where('schoolprofile_id', $id)->get();
 			$schoolprofile->otherImages = $school_images;
  			return response()->json(['success' => $schoolprofile], $this->successStatus);
 		} else {
